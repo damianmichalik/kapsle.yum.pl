446a3535dd4f17d9d7e27e7796fbaf7ab8a3cb7a
Damian Michalik <damian.michalik@gmail.com>
2015-01-30 23:09:05 +0100

    test

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,11 +1,16 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :application, "KAPSLE"
+set :domain,      "kapsle.yum.pl"
+set :deploy_to,   "/home/michol12/public_html/kapsle.yum.pl"
 set :app_path,    "app"
+set :user,        "michol12"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set  :deploy_via,    :copy
+#set :repository,  "git@bitbucket.org:damianmichalik/kapsle.yum.pl.git"
+set :repository,  "file:///var/www/kapsle.yum.pl"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :copy_vendors, true
+set :use_composer, false
 
 set :model_manager, "doctrine"
 # Or: `propel`
@@ -14,6 +19,8 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
