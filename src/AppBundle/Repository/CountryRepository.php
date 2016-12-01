94fe893dfdbf2f16941d0e9b2aab6f810ff13585
Damian Michalik <damian.michalik@ampmedia.pl>
2016-12-01 21:10:33 +0100

    Poprawa wyswietlania ilosci kapsli i browarow dla krajow

--- src/AppBundle/Repository/CountryRepository.php
+++ src/AppBundle/Repository/CountryRepository.php
@@ -38,13 +38,35 @@ class CountryRepository extends EntityRepository
 
     public function getAllCountries()
     {
+        $results = array();
+
         $qb = $this->createQueryBuilder('c')
-            ->select('count(b.id) AS num_breweries, count(k.id) AS num_caps, c.name, c.slug')
+            ->select('c.id, count(k.id) AS num_caps, c.name, c.slug')
             ->leftJoin('c.breweries', 'b')
             ->leftJoin('b.caps', 'k')
             ->groupBy('c.id')
             ->orderBy('c.name', 'ASC');
 
-        return $qb->getQuery()->getArrayResult();
+        $results1 = $qb->getQuery()->getArrayResult();
+
+        foreach($results1 as $result) {
+            $results[$result['id']] = $result;
+        }
+
+        $qb = $this->createQueryBuilder('c')
+            ->select('c.id, count(b.id) AS num_breweries, c.name, c.slug')
+            ->leftJoin('c.breweries', 'b')
+            ->groupBy('c.id')
+            ->orderBy('c.name', 'ASC');
+
+        $results2 = $qb->getQuery()->getArrayResult();
+
+        foreach($results2 as $result) {
+            if (array_key_exists($result['id'], $results)) {
+                $results[$result['id']]['num_breweries'] = $result['num_breweries'];
+            }
+        }
+
+        return $results;
     }
 }
